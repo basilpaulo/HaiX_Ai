cbf307ad01421f78ca76b00d1adf0f27b39dbc6a
basilpaulo <bazlpauloz@gmail.com>
2023-12-07 00:02:02 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <>
+      <Router>
+        <Home></Home>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
