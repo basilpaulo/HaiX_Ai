f540fbf00e7511cab172174d34004131fc0ad3b4
Alen Francis <alen.francis@clubready.com>
2023-12-07 00:08:40 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import Home from "./Components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
+
+import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div
-        style={{
-          width: "35vw",
-          height: "100vh",
-          border: "2px solid rgba(0, 0, 0, 0.05)",
-        }}
-      >
-        <Navbar></Navbar>
-      </div>
-      <div>
-        <Home></Home>
-      </div>
-    </div>
+    <>
+      <Router>
+        <div
+          style={{
+            width: "35vw",
+            height: "100vh",
+            border: "2px solid rgba(0, 0, 0, 0.05)",
+          }}
+        >
+          <Navbar></Navbar>
+        </div>
+        <div>
+          <Home></Home>
+        </div>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
