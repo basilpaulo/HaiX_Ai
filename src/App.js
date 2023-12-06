950151225bb8991d45ef631025b2a3737568e716
basilpaulo <bazlpauloz@gmail.com>
2023-12-07 01:41:58 +0530

    sidebar start

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Home from "./Components/Home/Home";
-import Navbar from "./Components/Navbar/Navbar";
-
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <div
-          style={{
-            width: "35vw",
-            height: "100vh",
-            border: "2px solid rgba(0, 0, 0, 0.05)",
-          }}
-        >
-          <Navbar></Navbar>
-        </div>
-        <div>
-          <Home></Home>
-        </div>
-        <Switch>
-          <Route path="/"></Route>
-        </Switch>
-      </Router>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import Navbar from "../src/Components/Navbar/Navbar.jsx";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
