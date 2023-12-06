fbbfe08dd6f5cfa5e117d438eda8e4585782afef
Alen Francis <alen.francis@clubready.com>
2023-12-06 23:48:45 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Home from "./Components/Home/Home";
+
 import "./App.css";
 import Home from "./Components/Home/Home";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <Home>
+      <Home></Home>
+    </Home>
+  );
 }
 
 export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../Assets/HaiX_white.png'
-import 'bootstrap/dist/css/bootstrap.css';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import React from "react";
+
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+
+import logo from "../Assets/HaiX_white.png";
+
+import "./Navbar.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Navbar = () => {
   return (
     <div>
-        <i class="bi bi-bookmark"></i>
-         <img className="logo" src={logo} alt='logo'></img>
-         
-         <ul className='list'><BookmarkBorderIcon className='bookmark'></BookmarkBorderIcon>Favourite Dashboard</ul>
+      <i className="bi bi-bookmark"></i>
+      <img className="logo" src={logo} alt="logo"></img>
+      <BookmarkBorderIcon />
+      <ul className="list">Favourite Dashboard</ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar   
+export default Navbar;
