907851d1edfa111d489dfe6023796be858818a0c
basilpaulo <bazlpauloz@gmail.com>
2023-12-07 21:12:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
-import Home from "../src/Components/Home/Home.jsx";
 
 function App() {
   return <div></div>;
-  <Home></Home>;
 }
 
 export default App;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import './Home.css'
-
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
+//import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-
 import SearchIcon from '@mui/icons-material/Search';
+import facebook from '../Assets/facebook.png'
+import instagram  from '../Assets/instagram.png'
+import tiktok  from '../Assets/tiktok.png'
+import youtube  from '../Assets/youtube.png'
+import twitter from '../Assets/twitter.png'
+import news  from '../Assets/news.png'
+import reddit  from '../Assets/reddit.png'
+import Avatars from './Avatar';
 
 
 
 export default function CustomizedInputBase() {
     return (
-        <div className='search'> 
-            <p class='title' >Stay ahed with Real-Time Insights</p>
+        <div className='search'>   
+            <p class='title' >Stay ahed with Real-Time Insights</p> <Avatars/>
       <Paper
         component="form"
         sx={{ p: '2px 400px', display: 'flex', alignItems: 'center', width: 400 }}
@@ -29,9 +35,21 @@ export default function CustomizedInputBase() {
         <IconButton type="button" sx={{ p: '10px 16rem' }} aria-label="search">
           <SearchIcon />
         </IconButton>
-    
-       
       </Paper>
+      <div className='social-logo'>
+      <img src={twitter} alt='instagram'></img>
+      <img src={instagram} alt='instagram'></img>
+      <img src={tiktok} alt='instagram'></img>
+      <img src={youtube} alt='instagram'></img>
+        <img src={facebook} alt='facebook'></img>
+        <img src={news} alt='instagram'></img>
+        <img src={reddit} alt='instagram'></img>
       </div>
+
+      <div>
+
+      </div>
+      </div>
+      
     );
   }
