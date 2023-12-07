91716c9e0deddcde1a8a194711b4c35bc71514cd
basilpaulo <bazlpauloz@gmail.com>
2023-12-07 10:07:07 +0530

    side bar commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import './Home.css'
 
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
 
-const Home = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+import SearchIcon from '@mui/icons-material/Search';
 
-export default Home
+
+
+export default function CustomizedInputBase() {
+    return (
+        <div className='search'> 
+            <p class='title' >Stay ahed with Real-Time Insights</p>
+      <Paper
+        component="form"
+        sx={{ p: '2px 400px', display: 'flex', alignItems: 'center', width: 400 }}
+      >
+        <IconButton sx={{ p: '1rem' }} aria-label="menu">
+          
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Start typing in any topic/company name"
+          inputProps={{ 'aria-label': 'Start typing in any topic/company name' }}
+        />
+        <IconButton type="button" sx={{ p: '10px 16rem' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+    
+       
+      </Paper>
+      </div>
+    );
+  }
